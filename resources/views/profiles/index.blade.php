52648b5c2d46166682500f4281126a9eb2d7afce
Colin Fleck <fleckc3@mail.dcu.ie>
2020-02-13 10:14:25 +0000

    Updated posts features, updted post controller, addded intervetnion library for image validation

---
+++ resources/views/profiles/index.blade.php
@@ -0,0 +1,44 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <div class="row">
+        <div class="col-3 p-5">
+        <img src="/svg/coligramlogo_colorlarge.svg" alt="">
+        </div>
+        <div class="col-9 pt-5">
+            <div class="d-flex justify-content-between align-items-baseline">
+                <h1>{{ $user->username }}</h1>
+                <a href="/p/create">Add new Post</a>
+            </div>
+            <div class="d-flex">
+                <div class="pr-5">
+                    <strong>{{ $user->posts->count() }}</strong> posts
+                </div>
+                <div class="pr-5">
+                    <strong>23k</strong> followers
+                </div>
+                <div class="pr-5">
+                    <strong>212</strong> following
+                </div>
+            </div>
+            <div class="pt-4 font-weight-bold">
+                {{ $user->profile->title }}
+            </div>
+            <div class="">
+                {{ $user->profile->bio }}
+            </div>
+            <div class="">
+                <a href="www.coligram.org">{{ $user->profile->url }}</a>
+            </div>
+        </div>
+        <div class="row pt-5">
+            @foreach($user->posts as $post)
+                <div class="col-4 pb-4">
+                    <img src="/storage/{{ $post->image }}" class="w-100">
+                </div>
+            @endforeach
+        </div>
+    </div>
+</div>
+@endsection
